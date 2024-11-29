1a07612c7ef4b7165d0aeb2af7b38219a1b93b85
vika84ch <vika3068769@gmail.com>
2024-11-29 23:35:27 +0300

    Update lamp.py

--- lamp.py
+++ lamp.py
@@ -1,22 +1,31 @@
 from flask import Flask, request
 import logging
 
+# Создаем экземпляр Flask-приложения
 app = Flask(__name__)
 
-# Настройка логирования
+# Настройка логирования с уровнем INFO и форматом, который включает время и сообщение
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
 
-LAMP_STATE = False  # Состояние фонаря (изначально выключен)
+# Состояние фонаря (изначально выключен)
+LAMP_STATE = False
 
+# Обработчик POST-запроса для управления состоянием фонаря
 @app.route('/lamp', methods=['POST'])
 def lamp():
     global LAMP_STATE
+    # Извлекаем состояние фонаря из JSON-тела запроса
     data = request.json
     state = data.get('state')
+    # Если новое состояние отличается от текущего, обновляем его
     if state != LAMP_STATE:
         LAMP_STATE = state
+        # Логируем новое состояние фонаря
         logging.info(f"Фонарь {'включен' if state else 'выключен'}")
+    # Возвращаем ответ "OK" с кодом 200
     return "OK", 200
 
+# Запуск сервера
 if __name__ == "__main__":
+    # Запускаем сервер Flask на порту 5001
     app.run(port=5001)
